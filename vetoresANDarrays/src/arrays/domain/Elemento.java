21af02506cdc0ab27e556e2fbdfdd4959475187b
Mateus <mateus@ufscar.br>
2024-02-22 16:31:44 -0300

    Criando as classes Elementos e ListaLigada

---
+++ vetoresANDarrays/src/arrays/domain/Elemento.java
@@ -0,0 +1,23 @@
+package arrays.domain;
+
+public class Elemento {
+    private String valor;
+    private Elemento proximo;
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Elemento getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Elemento proximo) {
+        this.proximo = proximo;
+    }
+}
+
---
+++ vetoresANDarrays/src/arrays/domain/ListaLigada.java
@@ -0,0 +1,41 @@
+package arrays.domain;
+
+public class ListaLigada {
+    private Elemento primeiro;
+    private Elemento ultimo;
+    private int tamanho;
+
+    public Elemento getPrimeiro() {
+        return primeiro;
+    }
+
+    public void setPrimeiro(Elemento primeiro) {
+        this.primeiro = primeiro;
+    }
+
+    public Elemento getUltimo() {
+        return ultimo;
+    }
+
+    public void setUltimo(Elemento ultimo) {
+        this.ultimo = ultimo;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public void adicionarElemento(String novoValor){
+
+    }
+    public void remover(String novoValor){
+
+    }
+    public Elemento get(int posicao){
+        return null;
+    }
+}
