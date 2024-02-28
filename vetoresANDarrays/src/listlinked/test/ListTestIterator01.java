8caca3abb8b75d5e91039173b1c33b2a646b90ff
Mateus <mateus@ufscar.br>
2024-02-28 15:32:44 -0300

    Estudo

--- vetoresANDarrays/src/listlinked/test/ListTestIterator01.java
+++ vetoresANDarrays/src/listlinked/test/ListTestIterator01.java
@@ -5,12 +5,14 @@ import listlinked.domain.IteratorList;
 import listlinked.domain.ListLinked;
 import listlinked.domain.NodeList;
 
+import java.util.ArrayList;
+
 public class ListTestIterator01 {
     public static void main(String[] args) {
         ListLinked<Integer> numbers = new ListLinked<>();
-        int count = 100;
+        int count = 10000000;
 
-        for (int i = 1; i <= count; i++){
+        for (int i = 1; i <= count; i++) {
             numbers.setElement(i);
         }
 
@@ -25,18 +27,32 @@ public class ListTestIterator01 {
         System.out.println("-------------------------------");
 
         double initialTime = System.currentTimeMillis();
-        numbers.getElement(1000000);
+        numbers.getElement(999999);
         double finalTime = System.currentTimeMillis();
         double totalTime = finalTime - initialTime;
 
         System.out.println("Time to find element: " + totalTime);
         System.out.println("-------------------------");
-        numbers.getall();
+
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 1; i <= count; i++) {
+            array.add(i);
+        }
+
+        initialTime = System.currentTimeMillis();
+        System.out.println(array.get(9999999));
+        finalTime = System.currentTimeMillis();
+        totalTime = finalTime - initialTime;
+        System.out.println("Time to find element: " + totalTime);
+        System.out.println("-------------------------");
+
+
+        /* numbers.getall();
         System.out.println("Size list: " + numbers.getSize());
         System.out.println("--------------------------");
         numbers.delElement(50);
         numbers.getall();
         System.out.println("--------------------------");
-        System.out.println("Size list: " + numbers.getSize());
+        System.out.println("Size list: " + numbers.getSize());*/
     }
 }
