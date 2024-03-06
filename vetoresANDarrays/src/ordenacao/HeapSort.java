71b4ce21daaa3bf4fe5e7f3374b2cc9c07574732
Mateus <mateus@ufscar.br>
2024-03-06 08:56:23 -0300

    Estudando Heap Sort

--- vetoresANDarrays/src/ordenacao/HeapSort.java
+++ vetoresANDarrays/src/ordenacao/HeapSort.java
@@ -6,22 +6,28 @@ public class HeapSort {
 
         System.out.println("Vetor desordenado!");
         System.out.println("---------------------------------------");
+        //criação do vetor
         for (int i = 0; i < vetor.length; i++) {
             vetor[i] = (int) (Math.random() * vetor.length);
-            System.out.print(vetor[i] + " ,");
+            System.out.print(vetor[i]+" ,");
         }
         System.out.println("\n---------------------------------------");
 
+        // 1ª parte - Ordenando os troncos e raiz da arvore binária
+
         int n = vetor.length;
-        for (int i = n / 2 - 1; i >= 0; i--) {
+        for(int i = n / 2 - 1; i >= 0; i--){
             ordenaHeapSort(vetor, n, i);
         }
 
-        for (int j = n - 1; j > 0; j--) {
+        // 2ª parte - Ordenando o restante e verificando novamente os troncos
+
+        for (int i = n - 1; i > 0; i--){
             int temp = vetor[0];
-            vetor[0] = vetor[j];
-            vetor[j] = temp;
-            ordenaHeapSort(vetor, j, 0);
+            vetor[0] = vetor[i];
+            vetor[i] = temp;
+            ordenaHeapSort(vetor, i, 0);
+
         }
 
         System.out.println("Vetor Ordenado!");
@@ -31,26 +37,28 @@ public class HeapSort {
         }
         System.out.println("\n---------------------------------------");
 
+        //
+
     }
+
     // Método que vai realizar a mudança de todos os nós e raiz principal da árvore binária
     private static void ordenaHeapSort(int[] vetor, int n, int i) {
         int raiz = i;
         int esq = 2 * i + 1;
         int dir = 2 * i + 2;
-
-        if (esq < n && vetor[esq] > vetor[raiz]) {
+        if (esq < n && vetor[esq] > vetor[raiz]){
             raiz = esq;
         }
-        if (dir < n && vetor[dir] > vetor[raiz]) {
+        if (dir < n && vetor[dir] > vetor[raiz]){
             raiz = dir;
         }
-        if (raiz != i) {
-            int temp = vetor[raiz];
-            vetor[raiz] = vetor[i];
-            vetor[i] = temp;
-
+        if (raiz != i){
+            int temp = vetor[i];
+            vetor[i] = vetor[raiz];
+            vetor[raiz] = temp;
             ordenaHeapSort(vetor, n, raiz);
         }
     }
-
 }
+
+
